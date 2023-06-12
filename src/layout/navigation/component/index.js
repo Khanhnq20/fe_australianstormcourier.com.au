b250fe4f487fbff0f48b700bb1e384572f940a94
Anonymous-alpha-linux <tinhntgcd18753@fpt.edu.vn>
2023-06-12 15:30:23 +0700

    tinh - complete the modification of register user and driver

--- src/layout/navigation/component/index.js
+++ src/layout/navigation/component/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
-import { NavAuth } from '../../../layout';
-import '../style/navigation.css';
-import { useContext } from 'react';
-import { AuthContext } from '../../../stores';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import { NavAuth } from "../../../layout";
+import "../style/navigation.css";
+import { useContext } from "react";
+import { AuthContext } from "../../../stores";
+import logo from "../../../image/as-logo.png";
 
 // {
 //     "accessToken": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxNzA4N2M5ZC01NDk5LTQ5YzEtOGY5Ny03MzU3MTQ2MzBmOWQiLCJlbWFpbCI6InRydW5ndGluaDI0NjgxMGZAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJzdWIiOiJBdXN0cmFsaWFuIFN0b3JtIFN1YmplY3QiLCJqdGkiOiI1Y2RiMzc0Ny0zMGQ0LTRlNzUtYTEyNC0zNTBiMzY0NDcwOGIiLCJpYXQiOjE2ODI4MzY5MzAsIm5iZiI6MTY4MjgzNjkzMCwiZXhwIjoxNjgyODY1NzMwLCJpc3MiOiJBdXN0cmFsaWFuIFN0b3JtIiwiYXVkIjoiaHR0cHM6Ly9hdXN0cmFsaWFuc3Rvcm1jb3VyaWVyLmNvbS5hdS8ifQ.Rg7SzMzR8wvbJUL3A3RBI-4jdjmAbAC4ezG6zzx0qsoND9GY8ZLYwdX3R_Aepkg18BHNGj-1AJEeHQ4E17FoYw",
@@ -35,30 +36,26 @@ import { AuthContext } from '../../../stores';
 export default function Index() {
     const [state] = useContext(AuthContext);
 
-    if(state.isLogged)
-        return <NavAuth></NavAuth>
+    if (state.isLogged) return <NavAuth></NavAuth>;
 
     return (
-        <div className='nav-root'>
+        <div className="nav-root">
             <Navbar bg="light" variant="light" expand="lg">
                 <Container className="nav-ctn-1">
                     <Navbar.Brand className="nav-logo-frame" href="/">
-                        <img src="https://australianstormcourier.com.au/wp-content/uploads/2023/04/as-logo.png" width="50px"/>
+                        <img src={logo} width="50px" />
                     </Navbar.Brand>
-                    
-                    <div className='nav-ctn-2'>
-                        <Link className='nav-link' to='/auth/login'>
-                            <h6 className='nav-txt-btn'>Login</h6> 
+
+                    <div className="nav-ctn-2">
+                        <Link className="nav-link" to="/auth/login">
+                            <h6 className="nav-txt-btn">Login</h6>
                         </Link>
-                        <Link className='nav-link' to='/auth/register/user'>
-                            <h6 className='nav-txt-btn'>Register</h6> 
+                        <Link className="nav-link" to="/auth/register/user">
+                            <h6 className="nav-txt-btn">Register</h6>
                         </Link>
                     </div>
                 </Container>
             </Navbar>
-        </div> 
-    )
+        </div>
+    );
 }
-
-
-
