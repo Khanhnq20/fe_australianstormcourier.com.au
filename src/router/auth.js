20374b620dca5404f7a2e1fafec4098afb583fea
Khanhnq20 <khanhnq20>
2023-06-13 10:45:07 +0700

    khanh - fix footer

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,92 +1,126 @@
 import { Outlet } from "react-router-dom";
-import { EmailCheck, EmailForgot, Forgot, Login, RegisterDriver, RegisterUser, ResetPassword, UserInformation, Verify } from "../pages";
+import {
+    EmailCheck,
+    EmailForgot,
+    Forgot,
+    Login,
+    RegisterDriver,
+    RegisterUser,
+    ResetPassword,
+    UserInformation,
+    Verify,
+} from "../pages";
 import { Footer } from "../layout";
 
 export const authChildrens = [
     {
-      path: "register",
-      element: <Outlet></Outlet>,
-      children: [
-        {
-          path: "user",
-          element: <>
-            <Outlet></Outlet>
-          </>,
-          children:[
+        path: "register",
+        element: <Outlet></Outlet>,
+        children: [
             {
-              path: "",
-              element: <>
-                <RegisterUser></RegisterUser>
+                path: "user",
+                element: (
+                    <>
+                        <Outlet></Outlet>
+                    </>
+                ),
+                children: [
+                    {
+                        path: "",
+                        element: (
+                            <>
+                                <RegisterUser></RegisterUser>
+                                <Footer></Footer>
+                            </>
+                        ),
+                    },
+                ],
+            },
+            {
+                path: "driver",
+                element: (
+                    <>
+                        <RegisterDriver />
+                        <Footer></Footer>
+                    </>
+                ),
+            },
+            {
+                path: "confirm",
+                element: (
+                    <>
+                        <EmailCheck></EmailCheck>
+                        <Footer.Custom></Footer.Custom>
+                    </>
+                ),
+            },
+            {
+                path: "verify",
+                element: (
+                    <>
+                        <Verify></Verify>
+                    </>
+                ),
+            },
+        ],
+    },
+    {
+        path: "login",
+        element: (
+            <>
+                <Login></Login>
                 <Footer></Footer>
-              </>
-            }
-          ]
-        },
-        {
-          path: "driver",
-          element: <>
-            <RegisterDriver />
-            <Footer></Footer>
-          </>
-        },
-        {
-          path: "confirm",
-          element: <>
-            <EmailCheck></EmailCheck>
-            <Footer.Custom></Footer.Custom>
-          </>
-        },
-        {
-          path:"verify",
-          element:<>
-            <Verify></Verify>
-          </>
-        }
-      ]
+            </>
+        ),
     },
     {
-      path: "login",
-      element:<>
-        <Login></Login>
-        <Footer.Custom></Footer.Custom>
-      </>
+        path: "forgot",
+        element: (
+            <>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </>
+        ),
+        children: [
+            {
+                path: "",
+                element: (
+                    <>
+                        <Forgot></Forgot>
+                    </>
+                ),
+            },
+            {
+                path: "email",
+                element: (
+                    <>
+                        <EmailForgot></EmailForgot>
+                    </>
+                ),
+            },
+        ],
     },
     {
-      path:"forgot",
-      element:<>
-        <Outlet></Outlet>
-        <Footer.Custom></Footer.Custom>
-      </>,
-      children:[{
-        path:'',
-        element:<>
-          <Forgot></Forgot>
-        </>
-      },
-      {
-        path:'email',
-        element:<>
-          <EmailForgot></EmailForgot>
-        </>
-      }
-    ]
+        path: "reset",
+        element: (
+            <>
+                <ResetPassword></ResetPassword>
+                <Footer></Footer>
+            </>
+        ),
     },
     {
-      path:"reset",
-      element: <>
-        <ResetPassword></ResetPassword>
-        <Footer.Custom></Footer.Custom>
-      </>
+        path: "verify",
+        element: <Outlet></Outlet>,
+        children: [
+            {
+                path: "",
+                element: <Verify></Verify>,
+            },
+            {
+                path: "phone",
+                element: <></>,
+            },
+        ],
     },
-    {
-      path:"verify",
-      element:<Outlet></Outlet>,
-      children: [{
-        path: '',
-        element: <Verify></Verify>
-      },{
-        path: 'phone',
-        element: <></>
-      }]
-    }
-  ]
+];
