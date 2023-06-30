45af1efc7ba78358035de230dfb62fdd8f6cc2d9
Khanhnq20 <khanhnq20>
2023-06-30 10:09:32 +0700

    connect api print invoice

--- src/pages/user create product/component/totalInvoice.js
+++ src/pages/user create product/component/totalInvoice.js
@@ -2,7 +2,16 @@ import React, { useRef } from 'react';
 import logo from '../../../image/as-logo.png';
 import { useReactToPrint } from 'react-to-print';
 
-export default function TotalInvoice({ values }) {
+export default function TotalInvoice({
+    destination,
+    itemName,
+    weight,
+    quantity,
+    receiverName,
+    receiverPhone,
+    packageType,
+    note,
+}) {
     const componentRef = useRef();
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
@@ -11,12 +20,7 @@ export default function TotalInvoice({ values }) {
     });
     return (
         <div className="t-invoice-root">
-            <div className="w-100" style={{ textAlign: 'center', padding: '20px', paddingBottom: '0' }}>
-                <button className="my-btn-yellow" onClick={handlePrint}>
-                    Print Invoice
-                </button>
-            </div>
-            <div className="t-invoice-form" ref={componentRef}>
+            <div className="t-invoice-form m-2 p-3" ref={componentRef} style={{ border: '1px solid #010101' }}>
                 <div>
                     <div className="t-invoice-header">
                         <div style={{ width: '70px' }}>
@@ -28,43 +32,48 @@ export default function TotalInvoice({ values }) {
                 <div className="t-invoice-to">
                     <span className="t-invoice-to-label">To</span>
                     <span className="t-invoice-to-content">
-                        <p className="t-invoice-to-child">Unit Number</p>
-                        <p className="t-invoice-to-child">Street Number</p>
-                        <p className="t-invoice-to-child">Street Name</p>
-                        <div className="t-invoice-form-suburb">
-                            <span className="t-invoice-to-child">suburb</span>
-                            <span className="t-invoice-to-child">state</span>
-                            <span className="t-invoice-to-child">post code</span>
-                        </div>
+                        <p className="t-invoice-to-child">{destination}</p>
+                        {/* <p className="t-invoice-to-child">{streetNumber}</p>
+                        <p className="t-invoice-to-child">{streetName}</p> */}
+                        {/* <div className="t-invoice-form-suburb">
+                            <span className="t-invoice-to-child">{suburb}</span>
+                            <span className="t-invoice-to-child">{state}</span>
+                            <span className="t-invoice-to-child">{postcode}</span>
+                        </div> */}
                     </span>
                 </div>
                 <div className="t-invoice-item">
                     <div className="t-invoice-form-suburb">
                         <span>
-                            Item Name: <b>People</b>
+                            Item Name: <b>{itemName}</b>
                         </span>
                         <span>
-                            Weight: <b>100kg</b>
+                            Weight: <b>{weight}</b>
                         </span>
                         <span>
-                            Quantity: <b>2</b>
+                            Quantity: <b>{quantity}</b>
                         </span>
                     </div>
                     <div className="t-invoice-form-suburb">
                         <span>
-                            Receiver Name: <b>Nguyen Khanh</b>{' '}
+                            Receiver Name: <b>{receiverName}</b>{' '}
                         </span>
                         <span>
-                            Receiver Phone: <b>+84 122 122 122</b>
+                            Receiver Phone: <b>{receiverPhone}</b>
                         </span>
                         <span>
-                            Package Type: <b>Special Item</b>
+                            Package Type: <b>{packageType}</b>
                         </span>
                     </div>
                     <div>
-                        Note : <b>loading.....</b>
+                        Note : <b>{note}</b>
                     </div>
                 </div>
+            </div>
+            <div className="w-100" style={{ textAlign: 'center', padding: '20px', paddingBottom: '0' }}>
+                <button className="my-btn-yellow mb-2" onClick={handlePrint}>
+                    Print Label
+                </button>
             </div>
         </div>
     );
